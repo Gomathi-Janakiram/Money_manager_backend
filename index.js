a79cb25f331bd5ffde23ea62b85fddd36ca194a0
Gomathi <gomathirengavilas@gmail.com>
2020-12-28 23:13:04 +0530

    add

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const mongodb = require("mongodb")
+const mongoClient = mongodb.MongoClient;
+const app = express()
+const cors = require("cors")
+
+const port = process.env.port || 8000
+const db_url = "mongodb+srv://admin:PEWkVfmBg4jlEGfm@cluster0.ejolf.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("Thank you")
+})
+
+app.get("/get", async (req, res) => {
+    try {
+        const clientInfo = await mongoClient.connect(db_url)
+        const db = clientInfo.db("money")
+        const data = await db.collection("transaction").find().toArray()
+        res.json({ data })
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+app.post("/post", async (req, res) => {
+    try {
+        const clientInfo = await mongoClient.connect(db_url)
+        const db = clientInfo.db("money")
+        const data=await db.collection("transaction").insertOne(req.body)
+        res.json({message:"created"})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+app.listen(port,(err)=>{
+    console.log(`listening to ${port}`)
+})
